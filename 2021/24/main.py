7c71c0550610867f70ca38631b28f894dadd6a79
Jeremy Neiman <docmarionum1@gmail.com>
2021-12-25 12:28:11 -0500

    comments

--- 2021/24/main.py
+++ 2021/24/main.py
@@ -1,16 +1,33 @@
 import itertools
 
+# The program is 14 blocks which each follow the same pattern:
+# inp w
+# mul x 0
+# add x z
+# mod x 26
+# div z <a>
+# add x <b>
+# eql x w
+# eql x 0
+# mul y 0
+# add y 25
+# mul y x
+# add y 1
+# mul z y
+# mul y 0
+# add y w
+# add y <c>
+# mul y x
+# add z y
+
+# I manually got these constants from the input by using that pattern
 a = (1, 1, 1, 1, 26, 26, 1, 26, 1, 26, 1, 26, 26, 26)
 b = (11, 11, 14, 11, -8, -5, 11, -13, 12, -1, 14, -5, -4, -8)
 c = (1, 11, 1, 11, 2, 9, 7, 11, 6, 15, 7, 1, 8, 6)
 
-def calc_z(z, i, model_number):
-    o = z%26 + b[i]
-    w = model_number[i]
-    ne = (o != w)
-    return (z//a[i])*(25*ne + 1) + ne*(w + c[i])
-
 def solve(default, product):
+    # These 7 digits we will enumerate in either decending or acsending order to find the first
+    # match
     for w0, w1, w2, w3, w6, w8, w10 in product:
         model_number = [
             w0, w1, 
@@ -25,12 +42,16 @@ def solve(default, product):
         z = 0
 
         for i in range(14):
+            # For digits where b is less than 0, we have a chance to reduce z, in which case, pick the
+            # largest digit such that we can make model_number[i] == o
             o = z%26 + b[i]
-
             if (b[i] < 11):
                 if (o > 0) and (o < 10):
                     model_number[i] = o
 
+            # The above instructions simplify to this formula that takes
+            # the current z and returns a new z given the constants above
+            # and the current model number
             w = model_number[i]
             ne = (o != w)
             z = (z//a[i])*(25*ne + 1) + ne*(w + c[i])
